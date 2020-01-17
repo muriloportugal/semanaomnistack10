bf6cb6a327c0a12ab838ca8c2d31cdd72364769b
Murilo Portugal <murilo.portugal@gmail.com>
2020-01-17 17:56:59 -0300

    Finalizado Interface Web

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import api from './services/api';
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 import './global.css';
 import './App.css';
@@ -16,141 +18,36 @@ import './Main.css';
 
 
 function App() {
+  const [devs, setDevs] = useState([]);
 
-  const [github_username, setGithubusername] = useState('');
-  const [techs, setTechs] = useState('');
+  useEffect(() => { // Busca no BD os Devs para serem exibidos na página inicial
+    async function loadDevs(){
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    };
+    loadDevs();
+  },[]); // Array vazio no final para executra somente uma vez quando carregar este componente.
 
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
-
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition( (position) => {
-      const { latitude, longitude } = position.coords;
-      setLatitude(latitude);
-      setLongitude(longitude);
-    }, (err) => {
-      console.log(err);
-    }, {
-      timeout: 30000,
-    });
-  },[]);
-
-
-  async function handleAddDev(e){
-    e.preventDefault();
-    const response = api.post('/devs',{
-      github_username,
-      techs,
-      latitude,
-      longitude
-    });
-    console.log(response.data);
+  async function handleAddDev(data){
+    const response = await api.post('/devs', data);
+    
+    setDevs([...devs, response.data]);
   }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form onSubmit={handleAddDev}>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do GitHub</label>
-            <input
-              name="github_username"
-              id="github_username"
-              required
-              value={github_username}
-              onChange={ e => setGithubusername(e.target.value)}
-            />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input
-              name="techs"
-              id="techs"
-              required
-              value={techs}
-              onChange={ e => setTechs(e.target.value)}
-            />
-          </div>
-          
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input
-                type="number"
-                name="latitude"
-                id="latitude"
-                required value={latitude}
-                onChange={ e => setLatitude(e.target.value)}
-              />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input
-                type="number"
-                name="longitude"
-                id="longitude"
-                required value={longitude}
-                onChange={ e => setLongitude(e.target.value)}
-              />
-            </div>
-          </div>
-          
-          <button type="submit">Salvar</button>
-        
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
+      
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/40072024?s=400&v=4" alt="Murilo Portugal"/>
-              <div className="user-info">
-                <strong>Murilo Portugal</strong>
-                <span>ABAP, ReactJS, NodeJS</span>
-              </div>
-            </header>
-            <p>Biografia, blablabla blablabla blablablablablabla blablabla blablabla blablablablablabla blablabla</p>
-            <a href="https://github.com/muriloportugal">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/40072024?s=400&v=4" alt="Murilo Portugal"/>
-              <div className="user-info">
-                <strong>Murilo Portugal</strong>
-                <span>ABAP, ReactJS, NodeJS</span>
-              </div>
-            </header>
-            <p>Biografia, blablabla blablabla blablablablablabla blablabla blablabla blablablablablabla blablabla</p>
-            <a href="https://github.com/muriloportugal">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/40072024?s=400&v=4" alt="Murilo Portugal"/>
-              <div className="user-info">
-                <strong>Murilo Portugal</strong>
-                <span>ABAP, ReactJS, NodeJS</span>
-              </div>
-            </header>
-            <p>Biografia, blablabla blablabla blablablablablabla blablabla blablabla blablablablablabla blablabla</p>
-            <a href="https://github.com/muriloportugal">Acessar perfil no GitHub</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/40072024?s=400&v=4" alt="Murilo Portugal"/>
-              <div className="user-info">
-                <strong>Murilo Portugal</strong>
-                <span>ABAP, ReactJS, NodeJS</span>
-              </div>
-            </header>
-            <p>Biografia, blablabla blablabla blablablablablabla blablabla blablabla blablablablablabla blablabla</p>
-            <a href="https://github.com/muriloportugal">Acessar perfil no GitHub</a>
-          </li>
+          {devs.map( dev => {
+            return (
+              <DevItem key={dev._id} dev={dev} />
+            );
+          })}
         </ul>
       </main>
     </div>
